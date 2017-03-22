c93cca1894cbdae3660c20a3ea3302fa6077ed6e
Renzo Marchena <renzo.marchena@cignium.com>
2017-03-22 11:28:58 -0500

    Added for each to print time marks for the words detected

--- SpeechRecognitionTest/SpeechRecognitionTest/Program.cs
+++ SpeechRecognitionTest/SpeechRecognitionTest/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.Speech.Recognition;
+using Microsoft.Speech.Recognition.SrgsGrammar;
+using Microsoft.Speech;
+
 using System.Globalization;
 
 namespace ConsoleApplication7
@@ -10,22 +13,24 @@ namespace ConsoleApplication7
         {
             var c = new CultureInfo("en-US");
             // Create a new Speech Recognizer
-            SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine(c);
+            var recognizer = new SpeechRecognitionEngine(c);
              
             // Configure the input to the recognizer.
             recognizer.SetInputToWaveFile(@"C:\Users\rmarchena\Downloads\speech.wav");
 
             // Create Speech Recognition Grammar 
-            Choices keyPhrases = new Choices();
-            keyPhrases.Add(new string[] { "credit card number" });
+            //var keyPhrases = new Choices();
+            //keyPhrases.Add(new string[] { "credit card number", "could you please" });
+            var srgsDocument = new SrgsDocument(@"C:\Users\rmarchena\Source\Repos\renzomarchena\SpeechRecognitionTest\SpeechRecognitionTest\grammar.grxml");
 
-            GrammarBuilder gb = new GrammarBuilder();
+
+            /*GrammarBuilder gb = new GrammarBuilder();
             gb.Append(keyPhrases);
-
+            */
             // Create the Grammar instance.
-            Grammar g = new Grammar(gb);
-
-
+            // Grammar g = new Grammar(gb);
+            var g = new Grammar(srgsDocument);
+            
             // Load the Grammar into the Speech Recognizer
             recognizer.LoadGrammar(g);
 
@@ -38,11 +43,20 @@ namespace ConsoleApplication7
 
         static void Recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
-            //if (e.Result == null) return;
+            if (e.Result == null) return;
             Console.WriteLine("Speech recognized: " + e.Result.Text);
-            Console.WriteLine(" Start time: " + e.Result.Audio.AudioPosition.Seconds);
+            Console.WriteLine(" Start time: " + e.Result.Audio.AudioPosition);
             Console.WriteLine(" Duration: " + e.Result.Audio.Duration);
+
+            foreach (var word in e.Result.Words)
+            {
+                Console.WriteLine(word.Text);
+                Console.WriteLine(word.AudioPosition);
+                Console.WriteLine(word.AudioDuration);
+                
+            }
             Console.ReadLine();
+           
         }
 
 
